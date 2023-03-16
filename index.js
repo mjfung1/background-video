d974740f806a6732a54e1ac2fd2dee8fc7834d87
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2023-03-16 13:40:44 -0400

    add style

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const btn = document.querySelector(".btn");
 const video = document.querySelector(".background-video");
 const playPause = document.querySelector(".fa-solid");
 const preloader = document.querySelector(".preloader");
+const title = document.querySelector(".title");
 
 btn.addEventListener("click", () => {
     if (btn.classList.contains("pause")) {
@@ -18,6 +19,18 @@ btn.addEventListener("click", () => {
         playPause.classList.remove("fa-pause");
     }
 });
+
+title.addEventListener("click", () => {
+    if (title.classList.contains("nightlife")) {
+        title.classList.remove("nightlife");
+        title.innerHTML = "REWIND";
+        video.setAttribute("src", "rewind.mp4");
+    } else {
+        title.classList.add("nightlife");
+        title.innerHTML = "NIGHTLIFE";
+        video.setAttribute("src", "CITY.mp4");
+    }
+})
 
 window.addEventListener("load", () => {
     preloader.style.zIndex = "-2";
